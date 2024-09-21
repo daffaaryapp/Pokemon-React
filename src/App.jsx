3f48155210b854716f14b4bc021f66e5af5e2208
daffa <daffaryasatya28@gmail.com>
2024-09-21 11:28:12 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import PokemonList from "./assets/components/PokemonList/PokemonList";
+
+function App() {
+  return (
+    <>
+      <PokemonList />
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/components/PokemonList/PokemonList.jsx
@@ -0,0 +1,25 @@
+import pokemonJSON from "../../data/pokemon.json";
+import { useState, useEffect } from 'react';
+
+function PokemonList() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    setPokemons(pokemonJSON);
+  }, []);
+
+//   console.log(pokemons);
+
+  return (
+    <div>
+      <div className="list-pokemon">
+        {pokemons.map((item) => (
+            
+          <h1>{item.id} {item.name}</h1>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default PokemonList;
