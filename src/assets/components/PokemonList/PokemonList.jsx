21ac0482c776af22c40f0d88e92ceb6294e6a091
daffa <daffaryasatya28@gmail.com>
2024-09-21 14:53:36 +0700

    fin1sh

--- src/assets/components/PokemonList/PokemonList.jsx
+++ src/assets/components/PokemonList/PokemonList.jsx
@@ -1,29 +1,47 @@
 import pokemonJSON from "../../data/pokemon.json";
 import { useState, useEffect } from 'react';
+import "./PokemonList.css";
+import PokemonItem from '../PokemonItem/PokemonItem';
 
 function PokemonList() {
   const [pokemons, setPokemons] = useState([]);
+  const [filterPokemons, setFilterPokemons] = useState([]);
+  const handleSearch = (e) => {
+    let search = pokemons.filter((item) => {
+      return item.name.toLowerCase().includes(e.target.value);
+    });
+    setFilterPokemons(search);
+  };
 
-  //test
+
   useEffect(() => {
     setPokemons(pokemonJSON);
   }, []);
 
-//   console.log(pokemons);
-
   return (
     <div>
+      <h1>Daftar Pokémon</h1>
+      <input
+        type="text"
+        placeholder="cari pokemon..."
+        className="search"
+        onChange={handleSearch}
+      />
       <div className="list-pokemon">
-        {pokemons.map((item) => (
-            
-          <h1>{item.id} {item.name}</h1>
-        ))}
+        {filterPokemons.length === 0 ? (
+        <div>data tidak ditemukan</div>
+        ) : (
+          filterPokemons.map((item) => (
+          <PokemonItem 
+            key={item.id} 
+            pokemon={item} 
+          />
+        ))
+      )}
       </div>
     </div>
   );
 }
-
-//komen
 
 
 
